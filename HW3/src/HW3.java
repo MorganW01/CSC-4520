16085f036f395178fbd328545a5a7b45fb39ee13
Morgan <morganw2001@gmail.com>
2022-02-20 18:26:23 -0500

    progress

--- HW3/src/HW3.java
+++ HW3/src/HW3.java
@@ -1,15 +1,32 @@
+import java.util.*;
+
 public class HW3 {
-    //
-    public static void main(String []args){
 
-
-
-
-
+    public static ArrayList<Integer> heapSort(int arr[]){
+        //creating max heap by using reverse order in Priority Queue
+        PriorityQueue<Integer> heap = new PriorityQueue<>(Collections.reverseOrder());
+        for(int i=0; i< arr.length; i++) {
+            heap.add(arr[i]);
+        }
+        ArrayList<Integer> heaper = new ArrayList<>();
+        while(heap.size()>0)
+            heaper.add(heap.remove());
+        Collections.reverse(heaper);
+        return heaper;
+    }
+    public static void printer(ArrayList<Integer> arr, int n) {
+        for(int i = 0; i < n; i++)
+            System.out.print(arr.get(i) + " ");
     }
 
+    public static void main(String args []) {
 
-
-
-
+        int testcase1[] = {1, 4, 5, 1, 3, 4, 2, 6};
+        ArrayList<Integer> test1 = heapSort(testcase1);
+        printer(test1, test1.size());
+        System.out.println();
+        int testcase2[] = {99, 23, 412, 56, 23, 49, 523};
+        ArrayList<Integer> test2 = heapSort(testcase2);
+        printer(test2, test2.size());
+    }
 }
