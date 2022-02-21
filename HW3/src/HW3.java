55131318c302c00cc752a03297335be93714492d
Morgan <morganw2001@gmail.com>
2022-02-20 22:02:44 -0500

    done

--- HW3/src/HW3.java
+++ HW3/src/HW3.java
@@ -1,32 +1,79 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
 
-public class HW3 {
+class HW3 {
+    public static void heapify(ArrayList<Integer> hT, int i) {
+        int size = hT.size();
+        int largest = i;
+        int l = 2 * i + 1;
+        int r = 2 * i + 2;
+        if (l < size && hT.get(l) > hT.get(largest))
+            largest = l;
+        if (r < size && hT.get(r) > hT.get(largest))
+            largest = r;
 
-    public static ArrayList<Integer> heapSort(int arr[]){
-        //creating max heap by using reverse order in Priority Queue
-        PriorityQueue<Integer> heap = new PriorityQueue<>(Collections.reverseOrder());
-        for(int i=0; i< arr.length; i++) {
-            heap.add(arr[i]);
+        if (largest != i) {
+            int temp = hT.get(largest);
+            hT.set(largest, hT.get(i));
+            hT.set(i, temp);
+
+            heapify(hT, largest);
         }
-        ArrayList<Integer> heaper = new ArrayList<>();
-        while(heap.size()>0)
-            heaper.add(heap.remove());
-        Collections.reverse(heaper);
-        return heaper;
-    }
-    public static void printer(ArrayList<Integer> arr, int n) {
-        for(int i = 0; i < n; i++)
-            System.out.print(arr.get(i) + " ");
     }
 
-    public static void main(String args []) {
+    public static void heapAdd(ArrayList<Integer> hT, int newNum) {
+        int size = hT.size();
+        if (size == 0) {
+            hT.add(newNum);
+        } else {
+            hT.add(newNum);
+            for (int i = size / 2 - 1; i >= 0; i--) {
+                heapify(hT, i);
+            }
+        }
+    }
 
-        int testcase1[] = {1, 4, 5, 1, 3, 4, 2, 6};
-        ArrayList<Integer> test1 = heapSort(testcase1);
-        printer(test1, test1.size());
-        System.out.println();
-        int testcase2[] = {99, 23, 412, 56, 23, 49, 523};
-        ArrayList<Integer> test2 = heapSort(testcase2);
-        printer(test2, test2.size());
+    public static int extractMax(ArrayList<Integer> heap) {
+        int max = (heap.get(1));
+        heap.set(1, heap.size() - 1);
+        heapify(heap, 1);
+        return max;
+    }
+
+
+    public static ArrayList<Integer> heapSort (ArrayList<Integer> array) {
+        int  n = array.size();
+
+        ArrayList<Integer> heap = new ArrayList<>(n);
+
+        ArrayList<Integer> result = new ArrayList<>(n);
+        for (int i=0; i< n-1; i++){ // n-1
+            heapAdd(heap, (Integer)array.get(i));
+
+        }
+
+        while(heap.size() > 0){
+            result.add(extractMax(heap));
+        }
+
+        Collections.reverse(result);
+        return result;
+
+    }
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> array = new ArrayList<Integer>();
+        array.add(1);
+        array.add(4);
+        array.add(5);
+        array.add(1);
+        array.add(3);
+        array.add(4);
+        array.add(2);
+        array.add(6);
+
+        heapSort(array);
+
     }
 }
