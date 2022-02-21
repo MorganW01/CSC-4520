555ec6d427bf243ea5c51a8196bc5951a2fc49d4
Morgan <morganw2001@gmail.com>
2022-02-21 12:36:36 -0500

    add comments; clean up code

--- HW3/src/HW3.java
+++ HW3/src/HW3.java
@@ -10,7 +10,8 @@ class HW3 {
         // define left and right nodes
         int left = (2 * i) + 1;
         int right = (2 * i) + 2;
-        //checks to see where to place left and right nodes
+
+        //checks left and right node values against the largest and assigns variables accordingly based on conditions.
         if (left < size && heap.get(left) > heap.get(largest))
             largest = left;
         if (right < size && heap.get(right) > heap.get(largest))
@@ -20,27 +21,33 @@ class HW3 {
             int temp = heap.get(largest);
             heap.set(largest, heap.get(i));
             heap.set(i, temp);
-
+            //recursive call to heapify
             heapify(heap, largest);
         }
     }
 
     public static void heapAdd(ArrayList<Integer> heap, int value) {
+        //stores the size of the array
         int size = heap.size();
-        //checks size of the array and adds values
-            heap.add(value);
-            for (int i = size / 2 - 1; i >= 0; i--) {
-                heapify(heap, i);
-            }
+        //adds the new value to the heap
+        heap.add(value);
+        //heapifies the heap with the new value
+        for (int i = size / 2 - 1; i >= 0; i--) {
+            heapify(heap, i);
         }
+    }
 
 
-    //where the problem is
     public static int extractMax(ArrayList<Integer> heap) {
+        //stores max: first element in the array at the index 0
         int max = heap.get(0);
+        //removes it from array
         heap.remove(0);
+        //fixes the size of the array
         heap.trimToSize();
+        //heapifies again
         heapify(heap, 0);
+        //returns max
         return max;
 
     }
@@ -61,14 +68,12 @@ class HW3 {
 
 
         //adds values of the heap from largest to smallest by extracting max each time
-
-        while(heap.size() > 0){ // *ISSUE IS HERE*
+        while(heap.size() > 0){
             result.add(extractMax(heap));
         }
 
-        //reverses the array so it's smallest to largest
+        //reverses the array so it's smallest to largest (max heap is largest to smallest by default)
         Collections.reverse(result);
-        //return result;
         return result;
 
 
@@ -77,17 +82,32 @@ class HW3 {
     public static void main(String[] args) {
         //driver method
 
-        ArrayList<Integer> array = new ArrayList<>();
-        array.add(1);
-        array.add(4);
-        array.add(5);
-        array.add(1);
-        array.add(3);
-        array.add(4);
-        array.add(2);
-        array.add(6);
+        //creates test case 1 array
+        ArrayList<Integer> testcase1 = new ArrayList<>();
+        //adds all elements
+        testcase1.add(1);
+        testcase1.add(4);
+        testcase1.add(5);
+        testcase1.add(1);
+        testcase1.add(3);
+        testcase1.add(4);
+        testcase1.add(2);
+        testcase1.add(6);
 
-        System.out.println(heapSort(array)); // *ISSUE IS HERE*
+        //creates test case 2 array
+        ArrayList<Integer> testcase2 = new ArrayList<>();
+        //adds all elements
+        testcase2.add(99);
+        testcase2.add(23);
+        testcase2.add(412);
+        testcase2.add(56);
+        testcase2.add(23);
+        testcase2.add(49);
+        testcase2.add(523);
+
+
+        System.out.println("Test case 1 result: "+ heapSort(testcase1));
+        System.out.println("Test case 2 result: "+ heapSort(testcase2));
 
     }
 }
